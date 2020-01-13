33a40413dca8af42aa4f99aace342e3503e38909
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2020-01-13 14:56:10 +0900

    Add デプロイ前まで追加

--- todo-api/config/routes.rb
+++ todo-api/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :v1 do
+    resources :todos, only: [:create, :destroy]
+    resources :users, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
